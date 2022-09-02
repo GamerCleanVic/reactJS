fbc6b5cb50fb1231b7c02e38a03b7d93e55b94b5
GamerCleanVic <jonassd2000@gmail.com>
2022-09-02 15:45:31 -0500

    insercaoDeObjetos&Tags

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() {     
+  const bloco = 
+    <div className={"bloco1"}>
+      <div><a href="#" className={'botao1'}>
+        YouTube
+      </a>
+      <a href="#" className={'botao1'}>
+        Twitch
+      </a>
+      <a href="#" className={'botao1'}>
+        Instagraum
+      </a></div>
+    </div>
+  ;
+  const element = <div>{bloco}</div>; 
+
+  //Função1 treino
+  function formatName(user){
+      return `${user.firstName} ${user.lastName}`;
+    }
+    const user = {
+      firstName: 'Harper',
+      lastName: 'Perez'
+    }
+    const element2 = (
+      <h1>
+        Hello, {formatName(user)}!
+      </h1>
+    );
+    //Fim Função 1
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {element2}
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className={"p1"}>
+          BOA TARDE!
+        </p>
+        <p>
+          {element}
         </p>
         <a
           className="App-link"
---
+++ src/header.js
@@ -0,0 +1,10 @@
+const header = 
+    <header className={"header1"}>
+        <a className={"botao1"}>
+            LOGIN
+        </a>
+        <a className={"botao1"}>
+            SENHA
+        </a>
+    </header>
+;
