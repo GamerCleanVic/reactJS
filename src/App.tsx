4f48adfb5d2ed1ad31fbf6c39fbd61a010d445e9
GamerCleanVic <jonassd2000@gmail.com>
2022-09-12 18:21:54 -0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Header} from './components/Header';
+const App = () => {  
+  return(    
+    <div className={"container-btn1"}>      
+      <Header />
+      <p className="parag1">
+        Muito obg por assistir!
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+    return(
+      <header className="headerVite">
+        <h1>
+          Título do Header
+        </h1>
+        <hr />
+      </header>
+    );
+}
