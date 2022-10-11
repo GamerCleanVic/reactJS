e434371fc2d6fb5697e3330892f22508a69c90a4
GamerCleanVic <jonassd2000@gmail.com>
2022-10-10 20:06:55 -0500

    +reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,48 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
-import * as C from './components/styled1/AppStyles';
-import {Post} from './components/types/Post';
+import {useReducer} from 'react';
 
+type reducerState = {
+  count: number;
+}
+type reducerAction = {
+  type: string;
+}
 
-const App = () => { 
-
-  const [posts, setPosts] = useState<Post[]>([]);
-  const loadPosts = async () => {
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    let json = await response.json();
-    setPosts(json);
+const initialState = {count: 0};
+const reducer = (state: reducerState, action: reducerAction) => {
+  switch(action.type){
+    case 'ADD':
+      return{...state, count: state.count + 1};
+    break;
+    case 'DEL':
+      return {...state, count: state.count -1};
+    break;
+    case 'RESET':
+      return initialState;
+    break;
   }
-  const [addTitleText, setAddTitleText] = useState(['']);
-  const [addBodyText, setAddBodyText] = useState(['']);
+  return state;
+}
 
-  useEffect(() => {
-    loadPosts();
-  }, []);
+const App = () => {
 
-  const handleAddTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setAddTitleText(e.target.value);
-  }
-  const handleAddBodyChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setAddBodyText(e.target.value);
-  }
-  const handleAddClick = () => {
-    alert(addTitleText+' - '+addBodyText);
-  }
-  return(  
-    <div className="
-    bg-blue-900
-      p-3
-      w-screen
-      h-11
-      fixed
-      top-0
-      left-0
-      uppercase
-      font-bold
-    ">
-      <div className="fixed top-auto left-3">   
-        <div className="bg-cyan-300 w-7 p-[2px] mb-1"></div>
-        <div className="bg-cyan-300 w-7 p-[2px] mb-1"></div>
-        <div className="bg-cyan-300 w-7 p-[2px]"></div>
-      </div>
-      <div className="
-        
-        right-0 
-      text-cyan-300
-        mr-3
-
-      ">        
-        <a href='#' className='no-underline'>ARTIGOS</a> 
-
-        <fieldset className="border-2 mb-3 p-3 mt-7">
-            <legend>Adicionar Novo Post</legend>
-            <input className="block border m-3"
-              value={addTitleText}
-              onChange={handleAddTitleChange}
-              type="text" 
-              placeholder="Digite um título"
-             />
-            <textarea className="block border
-              m-3"
-              value={addBodyText}
-              onChange={handleAddBodyChange} 
-             ></textarea>
-            <button className="block border
-             text-blue-700 m-3"
-                onClick={handleAddClick}
-             >
-              Adicionar
-            </button>
-        </fieldset>
-
-        <div className="mt-7">            
-            Total de posts: {posts.length}
-        </div>
-        <div>
-          {posts.map((item, index) => (
-            <div key={index}>
-              <h4 
-                className="
-                  text-blue-400
-                  text-[1.4rem]                  
-                "
-              >{item.title}</h4>
-              <small 
-                className="
-                  text-yellow-400                    
-                "
-              >#{item.id} - Usuário {item.userId}</small>
-              <p 
-                className="
-                  text-white
-                  text-[0.7rem]
-                  mb-5
-                "
-              >{item.body}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>      
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div className="p-5">
+      Contagem: {state.count}
+      <hr />
+      <button onClick={() => dispatch
+        ({type: 'ADD'})} className="mt-3 ml-3 mr-3 text-green-700">
+          Adicionar
+      </button>
+      <button onClick={() => dispatch
+        ({type: 'DEL'})} className="mt-3 ml-3 mr-3 text-green-700">
+          Deletar
+      </button>
+      <button onClick={() => dispatch
+        ({type: 'RESET'})} className="mt-3 ml-3 mr-3 text-green-700">
+          Resetar
+      </button>
+    </div>
   );
 }
 export default App;
---
+++ src/api.ts
@@ -0,0 +1,9 @@
+export const api = {
+    getAllPosts: async () => {
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        let json = await response.json();
+    }
+    addNewPost: () => {
+
+    }
+}
