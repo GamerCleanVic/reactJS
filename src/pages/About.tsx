d576aa838d6448c12c2eee96dcc05e1aa2e0c2d9
GamerCleanVic <jonassd2000@gmail.com>
2022-11-09 21:07:40 -0500

    +routeQuery

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,38 @@
-import {Link} from 'react-router-dom';
+import {Link, useSearchParams} from 'react-router-dom';
 
 export const About = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const setOrder = (order: 'asc' | 'desc') => {
+        searchParams.set('order', order);
+        setSearchParams(searchParams);
+    }
     return(
         <div>
-            About
+            Filtro: {searchParams.get('filter')}<br />
+            Ordem: {searchParams.get('order')}
+            <hr />
+            <button className='
+                pt-0
+                pb-0
+                m-2
+                text-green-700
+            '
+            onClick={()=>setOrder('asc')}
+            >
+                Asc
+            </button>
+            <button className='
+                pt-0
+                pb-0
+                m-2
+                text-green-700
+            '
+            onClick={()=>setOrder('desc')}
+            >
+                Desc
+            </button>
+            <hr />
+            Página sobre:
             <ul>
                 <li><Link to="/sobre/joao">João</Link></li>
                 <li><Link to="/sobre/pedro">Pedro</Link></li>
--- src/pages/AboutItem.tsx
+++ src/pages/AboutItem.tsx
@@ -1,10 +1,36 @@
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
 
 export const AboutItem = () => {
     const params = useParams();
+    const navigate = useNavigate();
+    const handleBackButton = () => {
+        navigate(-1);
+    }
+    const handleHomeButton = () => {
+        navigate('/');
+    }
     return(
         <div>
             Página sobre {params.slug?.toUpperCase()} ({params.slug?.length} letras)
+            <hr />
+            <button className='mt-3
+                pt-0
+                pb-0 
+                text-green-700'
+                onClick={handleBackButton}
+            >
+                Voltar
+            </button>
+            <button className='
+                    pt-0
+                    pb-0
+                    ml-3
+                    text-green-700
+                    uppercase'
+                    onClick={handleHomeButton}
+            >
+                Home
+            </button>
         </div>
     );
 }
