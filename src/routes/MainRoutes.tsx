d370109f535c1f6fd9617fe85b1cf17034487b4e
GamerCleanVic <jonassd2000@gmail.com>
2022-11-22 17:28:44 -0500

    +useRoutes

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,10 +1,17 @@
-import {Routes, Route} from 'react-router-dom';
+import {useRoutes} from 'react-router-dom';
 import {Home} from '../pages/Home';
 import {About} from '../pages/About';
 import {AboutItem} from '../pages/AboutItem';
 import {NotFound} from '../pages/NotFound';
 import RequireAuth from '../RequireAuth';
 export const MainRoutes = () => {
+    return useRoutes([
+        { path: '/', element: <Home /> },
+        { path: '/sobre', element: <RequireAuth><About /></RequireAuth> },
+        { path: '/sobre/:slug', element: <AboutItem /> },
+        { path: '*', element: <NotFound /> }
+    ]);
+    /*
     return(
         <Routes>
             <Route path="/" element={<Home />} />
@@ -12,5 +19,5 @@ export const MainRoutes = () => {
             <Route path="/sobre/:slug" element={<AboutItem />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
-    );
+    ); */
 }
